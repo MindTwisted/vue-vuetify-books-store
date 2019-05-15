4af1c3b0e01642ffab4f82d2c9405095231be793
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2019-05-15 17:31:42 +0300

    fix: code style rules;

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,17 @@ module.exports = {
     env: {
         node: true
     },
-    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/standard'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        indent: ['error', 4]
+        'indent': ['error', 4],
+        'semi': ['error', 'always'],
+        'space-before-function-paren': ['error', {
+            'anonymous': 'never',
+            'named': 'never',
+            'asyncArrow': 'never'
+        }]
     },
     parserOptions: {
         parser: 'babel-eslint'
