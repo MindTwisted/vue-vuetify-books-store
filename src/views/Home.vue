22c6b01f0370e73ce3d6f5ff3c1fcf4e31811315
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2019-05-16 17:03:57 +0300

    add: home view layout;

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="home">
-        Home
+        <v-card>
+            <v-responsive min-height="500">
+                <v-container fill-height>
+                    <v-layout align-center>
+                        <v-flex>
+                            <h3 class="display-3">Lorem, ipsum dolor.</h3>
+
+                            <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+
+                            <v-divider class="my-3"></v-divider>
+
+                            <div class="title mb-3">Lorem ipsum dolor sit amet.</div>
+
+                            <v-btn
+                                :to="{ name: 'shop' }"
+                                class="mx-0"
+                                color="primary"
+                                large
+                            >
+                                See shop
+                            </v-btn>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-responsive>
+        </v-card>
     </div>
 </template>
 
